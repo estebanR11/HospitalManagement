669e8646c2feb256b7c75742ce1a64e1f6e7946b
Esteban Rubio <rubioesteban1107@gmail.com>
2024-01-16 09:50:20 -0500

    first

---
+++ Hosppital/Model/Context/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    public partial class DBConnection:DbContext
+    {
+        private DBConnection(string stringConnection)
+            :base(stringConnection)
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+            this.Database.CommandTimeout = 900;
+        }
+
+        public static DBConnection Create()
+        {
+            return new DBConnection("name=DbConnection");
+        }
+    }
+}
